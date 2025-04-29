662a0ef3028244858479d872ef8d1f3501744c2d
sanjeev955 <sanjeevs13711@gmail.com>
2025-04-29 11:19:57 +0530

    save

---
+++ ArithmeticOperations.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperations {
+    public ArithmeticOperations() {
+    }
+ 
+    public static void main(String[] var0) {
+       byte var1 = 10;
+       byte var2 = 5;
+       int var3 = var1 + var2;
+       int var4 = var1 - var2;
+       int var5 = var1 * var2;
+       int var6 = var1 / var2;
+       int var7 = var1 % var2;
+       System.out.println("Number 1: " + var1);
+       System.out.println("Number 2: " + var2);
+       System.out.println("Sum: " + var3);
+       System.out.println("Difference: " + var4);
+       System.out.println("Product: " + var5);
+       System.out.println("Quotient: " + var6);
+       System.out.println("Remainder: " + var7);
+    }
