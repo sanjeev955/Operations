81a0c19d4dfd6a1f47bc0e42ee6642ab626b93d2
sanjeev955 <sanjeevs13711@gmail.com>
2025-04-29 11:24:53 +0530

    save

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -10,6 +10,25 @@ public class ArithmeticOperations {
        int var5 = var1 * var2;
        int var6 = var1 / var2;
        int var7 = var1 % var2;
+       double power = Math.pow(var1, var2);
+        System.out.println("Power (num1 ^ num2): " + power);
+
+        // Square root
+        if (var1 >= 0) {
+            double sqrtNum1 = Math.sqrt(var1);
+            System.out.println("Square root of num1: " + sqrtNum1);
+        } else {
+            System.out.println("Square root of num1: Cannot calculate square root of a negative number");
+        }
+
+        if (var2 >= 0) {
+            double sqrtNum2 = Math.sqrt(var2);
+            System.out.println("Square root of num2: " + sqrtNum2);
+        } else {
+            System.out.println("Square root of num2: Cannot calculate square root of a negative number");
+        }
+        
+    
        System.out.println("Number 1: " + var1);
        System.out.println("Number 2: " + var2);
        System.out.println("Sum: " + var3);
@@ -17,4 +36,5 @@ public class ArithmeticOperations {
        System.out.println("Product: " + var5);
        System.out.println("Quotient: " + var6);
        System.out.println("Remainder: " + var7);
-    }
+    }}
+    
